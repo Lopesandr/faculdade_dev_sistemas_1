48780da7b8311bb81d63d68798f61db98a26a93a
Andressa Lopes <dessarosalopes15@gmail.com>
2025-06-02 22:27:48 -0300

    Aula no dia 02/06/2025

--- dev_aulas/strava/urls.py
+++ dev_aulas/strava/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-import strava.views.exemplo as views_
+import strava.views.exemplo  as views_
 import strava.views.estatica as views 
 
 from strava.views import * 
@@ -12,7 +12,14 @@ urlpatterns = [
     path('classe_saudacao', SaudacaoView.as_view(), name="classe_saudacao_view"),
     path('funcao/<str:name>', views.nome, name="nome"),
     path('classe_nome/<str:name>', NomeView.as_view(), name="classe_nome"),
-    path('exemplo/fuction',views_.exemplo_list , name="exemplo_fuction_list")
+    path('exemplo/fuction',views_.exemplo_list , name="exemplo_fuction_list"),
+    path('exemplo/fuction/read/<int:pk>', views_.exemplo_detail, name='exemplo_fuction_read'),
+    path('exemplo/classe/read/<int:pk>', views_.ExemploDetailView.as_view(), name='exemplo_classe_read'),
+    path('exemplo/fuction/delete/<int:exemplo_id>', views_.delete, name='exemplo_fuction_delete'),
+    path('exemplo/class/delete/<int:exemplo_id>', views_.ExemoloDeleteView.as_view(), name='exemplo_class_delete'),
+
+
+
     # path('exemplo/classe', ExemploList.as_view(), name="exemplo_classe_list")
 
 ]
--- dev_aulas/strava/views/exemplo.py
+++ dev_aulas/strava/views/exemplo.py
@@ -1,10 +1,53 @@
+from django.urls import reverse_lazy
 from strava.models.perfil import Pefil
-from django.shortcuts import render, redirect
-from django.core.serializers import serialize 
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.core.serializers import serialize
 from strava.models.perfil import Pefil
+from django.views import View
+from django.views.generic import DeleteView
+
+
+
 
 
 def exemplo_list(request):
     exemplos = Pefil.objects.all()
     context = {'exemplos': exemplos,}
     return render(request,  'exemplo/list.html', context)
+
+def exemplo_detail(request, pk):
+    exemplo = Pefil.objects.get(id=pk)
+    context = {'exemplo':exemplo,}
+    return render(request, 'exemplo/read.html', context)
+
+def delete(request, exemplo_id):
+    exemplo = get_object_or_404(Pefil, pk=exemplo_id)
+    print
+    try: 
+        if request.method == 'POST':
+            v_pk_id = request.POST.get("exemplo_id", None)
+            print(v_pk_id)
+            if int(v_pk_id) == exemplo_id:
+                exemplo.delete()
+                return redirect('strava:exemplo_fuction_list')
+        else:
+            context = {'exemplo': exemplo}
+            return render(request, 'exemplo/delete.html', context)
+    except Exception as e:
+        print(e)
+        context = {}
+        return render(request, 'exemplo/read.html', context)
+
+class ExemploDetailView(View):
+    @staticmethod
+    def get(request, pk): 
+        exemplo = Pefil.objects.get(id=pk)
+        context = {'exemplo':exemplo,}
+        return render(request, 'exemplo/read.html', context)
+
+
+class ExemoloDeleteView(DeleteView):
+    model = Pefil
+    fields = '__all__'
+    template_name = "exemplo/delete.html"
+    sucess_url = reverse_lazy('strava:exemplo_fuction_list')
